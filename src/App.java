8354c5833f032ca4cf14395737f480aec531c3c4
Luís Gustavo <githlima3105@gmail.com>
2025-08-20 13:56:44 -0300

    test

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("JV Bixa de Maven");
+        Label label = new Label("Eu não Preciso de Maven para fazer um app");
         Scene scene = new Scene(label, 400, 200); 
 
 
